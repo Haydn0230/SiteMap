479745b85635796cc79e6fb99f2ba81d509533ea
Haydn0230 <haydnb.jones@gmail.com>
2020-11-06 13:16:37 +0000

    [SM-001] test bed set up

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Haydn0230/golessons/SiteMap
 
 go 1.14
+
+require (
+	github.com/Haydn0230/Link v0.0.0-20201106125920-6b1188156fdf
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+)
--- main.go
+++ main.go
@@ -1,42 +1,58 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"github.com/Haydn0230/golessons/parse/Link"
-
-	"log"
-	"net/http"
+	"github.com/Haydn0230/Link"
+	"io/ioutil"
 )
+
+type options struct {
+	depth int
+	url string
+}
+
+//stash.skybet.net
 func main() {
-	urlFlag :=  flag.String("url", "", "The site you want to build a site map for")
-	//depthFlag := flag.Int("depth", 2, "the depth of the links you want to explore. By default it will go 2 deep")
-	flag.Parse()
+	//op := getFlags()
 
-	fmt.Println(*urlFlag)
+	//res, err := http.Get(*urlFlag)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer res.Body.Close()
 
-	res, err := http.Get( *urlFlag)
-	if err != nil{
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-
-	//Link.HTML()
-	fmt.Printf("\n%v - %v\n", res.Status, res.StatusCode)
-
-
+	//fmt.Printf("\n%v - %v\n", res.Status, res.StatusCode)
 
 	//scanner := bufio.NewScanner(res.Body)
-	ll, err := Link.HTML(res.Body)
+	htmlDoc, err := ioutil.ReadFile("./calhoun.html")
 	if err != nil {
-		fmt.Printf("Error %v/n",err)
+		fmt.Printf("Error %+v/n",err)
+	}
+	r := bytes.NewReader(htmlDoc)
+	ll, err := Link.Parse(r)
+	if err != nil {
+		fmt.Printf("Error %+v/n",err)
 	}
 
-	fmt.Println(ll)
+	fmt.Printf("\n RESULTS \n %+v",ll)
 	//for scanner.Scan() {
 	//
 	//	fmt.Print(scanner.Text())
 	//}
 
 }
+
+
+func getFlags() options {
+	op := options{
+		url: *flag.String("url", "", "The site you want to build a site map for"),
+		depth: *flag.Int("depth", 2, "the depth of the links you want to explore. By default it will go 2 deep"),
+		}
+	//depthFlag := flag.Int("depth", 2, "the depth of the links you want to explore. By default it will go 2 deep")
+	flag.Parse()
+	return op
+}
+
+

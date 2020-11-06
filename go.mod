e0c6991c2004dcad79e01aa60bcbec736ea7ce93
Haydn0230 <haydnb.jones@gmail.com>
2020-11-06 12:32:14 +0000

    [SM-001] add in go dependencies

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Haydn0230/golessons/SiteMap
+
+go 1.14
--- main.go
+++ main.go
@@ -1,2 +1,42 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"github.com/Haydn0230/golessons/parse/Link"
+
+	"log"
+	"net/http"
+)
+func main() {
+	urlFlag :=  flag.String("url", "", "The site you want to build a site map for")
+	//depthFlag := flag.Int("depth", 2, "the depth of the links you want to explore. By default it will go 2 deep")
+	flag.Parse()
+
+	fmt.Println(*urlFlag)
+
+	res, err := http.Get( *urlFlag)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+
+	//Link.HTML()
+	fmt.Printf("\n%v - %v\n", res.Status, res.StatusCode)
+
+
+
+	//scanner := bufio.NewScanner(res.Body)
+	ll, err := Link.HTML(res.Body)
+	if err != nil {
+		fmt.Printf("Error %v/n",err)
+	}
+
+	fmt.Println(ll)
+	//for scanner.Scan() {
+	//
+	//	fmt.Print(scanner.Text())
+	//}
+
+}
